892400825e88b38cea305329791f01ef7fa2de3e
irem nur ceylan <ceylannnirem@outlook.com>
2021-04-25 19:06:47 +0300

    KampOdev

--- kampOdev/src/kampOdev/CourseManager.java
+++ kampOdev/src/kampOdev/CourseManager.java
@@ -3,10 +3,10 @@ package kampOdev;
 public class CourseManager {
 
  public void add(Course course) {
-	 System.out.println(course.courseName + "Eðitmeni:"+course.instructorName + ",Ýsimli kurs sisteme EKLENDÝ.." );
+	 System.out.println(course.courseName + "Eðitmeni:"+course.instructorName + ",Isimli kurs sisteme EKLENDI.." );
  }
  
  public void delete(Course course) {
-	 System.out.println(course.courseName + course.courseDetails + ",kursu sistemden SÝLÝNDÝ!!") ;
+	 System.out.println(course.courseName + course.courseDetails + ",kursu sistemden SILINDI!!") ;
  }
 }
--- kampOdev/src/kampOdev/Main.java
+++ kampOdev/src/kampOdev/Main.java
@@ -5,17 +5,17 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("---------KURSLAR-----------");
-		Course course1 = new Course(1," JAVA ","Yazýlým Geliþtrici Kampý(JAVA+REACT) " , "-> ENGÝN DEMÝROÐ");
-		Course course2 = new Course(2," C# ","Yazýlým Geliþtirici Kampý (C#+ANGULAR) ", "->  ENGÝN DEMÝROÐ");
+		Course course1 = new Course(1," JAVA ","Yazilim Geliþtrici Kampi(JAVA + REACT) " , "-> ENGIN DEMIROG");
+		Course course2 = new Course(2," C# ","Yazilim Geliþtirici Kampý (C# + ANGULAR) ", "->  ENGIN DEMIROG");
 		
 		Course[] courses = {course1,course2};	
 		for(Course course : courses) {
 			System.out.println(course.id + course.courseName + course.courseDetails+course.instructorName);
 		}
-		System.out.println("--------- EÐÝTMENLER---------------------");
-		Instructor instructor1 = new Instructor(1," Engin Demiroð");		
-		Instructor instructor2 = new Instructor (2," Ömer Ceylan");
-		Instructor instructor3 = new Instructor (3," Ýrem Ceylan");
+		System.out.println("--------- EGITMENLER---------------------");
+		Instructor instructor1 = new Instructor(1," Engin Demirog");		
+		Instructor instructor2 = new Instructor (2," Omer Ceylan");
+		Instructor instructor3 = new Instructor (3," Irem Ceylan");
 		
 		Instructor[] instructors = {instructor1,instructor2,instructor3};
 		for (Instructor instructor:instructors) {
